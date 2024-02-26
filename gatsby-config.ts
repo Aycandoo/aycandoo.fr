a2ad56445997be0365461df879b03085a4c75a34
Jérémy Brochard <jrmy.brochard@gmail.com>
2024-02-26 13:26:23 +0100

    feat: add gatbsy image plugin

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,15 +1,20 @@
-import type { GatsbyConfig } from "gatsby"
+import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: 'Aycandoo.fr',
-    siteUrl: `https://aycandoo.fr`,
+    siteUrl: 'https://aycandoo.fr',
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ['gatsby-plugin-postcss'],
-}
+  plugins: [
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+  ],
+};
 
-export default config
+export default config;
