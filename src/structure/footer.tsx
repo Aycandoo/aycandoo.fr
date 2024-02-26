2d5eff51099ec03f45353d76a10cdccec78dd7cc
Jérémy Brochard <jrmy.brochard@gmail.com>
2024-02-26 23:32:55 +0100

    feat: update footer

--- src/structure/footer.tsx
+++ src/structure/footer.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Footer = () => {
-  return <footer>&copy; Aycandoo</footer>;
+  return (
+    <footer className="bg-black text-white flex items-center justify-center h-20 w-full mt-32">
+      &copy; 2024 Aycandoo. All rights reserved
+    </footer>
+  );
 };
 
 export default Footer;
