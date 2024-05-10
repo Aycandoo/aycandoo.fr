7b937f07abb2a34fcbef35ec6ea77bd2486e0cb5
Jérémy Brochard <jrmy.brochard@gmail.com>
2024-05-10 16:56:59 +0200

    feat: update seo component to include description meta tag

--- src/structure/seo.tsx
+++ src/structure/seo.tsx
@@ -3,9 +3,10 @@ import React, { type FC } from 'react';
 
 export interface SeoParams {
   title: string;
+  description: string;
 }
 
-const Seo: FC<SeoParams> = ({ title }) => {
+const Seo: FC<SeoParams> = ({ title, description }) => {
   const data = useStaticQuery(graphql`
     query {
       site {
@@ -22,6 +23,7 @@ const Seo: FC<SeoParams> = ({ title }) => {
       <title>
         {title} | {data?.site?.siteMetadata?.title}
       </title>
+      <meta name="description" content={description} />
       <link rel="preconnect" href="https://rsms.me/" />
       <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
     </>
