019abc725b73ee683a0db4cca50033fffb8abfd0
Jérémy Brochard <jrmy.brochard@gmail.com>
2024-05-12 20:53:32 +0200

    refactor: update mobile menu (add transitions and fix not closing issue)

--- src/structure/header.tsx
+++ src/structure/header.tsx
@@ -1,4 +1,9 @@
-import { Dialog } from '@headlessui/react';
+import {
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+  Transition,
+} from '@headlessui/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import { Link, graphql, useStaticQuery } from 'gatsby';
 import { StaticImage } from 'gatsby-plugin-image';
@@ -25,7 +30,12 @@ const Header: FC = () => {
   `);
 
   return (
-    <header className="sticky inset-x-0 top-0 z-50 bg-gray-950">
+    <header
+      className={
+        'sticky inset-x-0 top-0 bg-gray-950 ' +
+        (isMobileMenuOpen ? 'z-0' : 'z-50')
+      }
+    >
       <nav
         className="flex items-center justify-between p-6 lg:px-8"
         aria-label="Global"
@@ -68,52 +78,68 @@ const Header: FC = () => {
         </div>
       </nav>
 
-      <Dialog
-        as="div"
-        className="lg:hidden"
-        open={isMobileMenuOpen}
-        onClose={setIsMobileMenuOpen}
+      <Transition
+        show={isMobileMenuOpen}
+        enter="transition-opacity duration-500"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="transition-opacity duration-300"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
       >
-        <div className="fixed inset-0 z-50" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
-              <StaticImage
-                className=""
-                height={40}
-                src="../images/aycandoo-black-logo.svg"
-                alt=""
-              />
-              <span className="sr-only">{data?.site?.siteMetadata?.title}</span>
-            </a>
-            <button
-              type="button"
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
-              onClick={() => {
-                setIsMobileMenuOpen(false);
-              }}
-            >
-              <span className="sr-only">Close menu</span>
-              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-            </button>
-          </div>
-          <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
-                {navigation.map(({ name, to }) => (
-                  <Link
-                    key={name}
-                    to={to}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                  >
-                    {name}
-                  </Link>
-                ))}
+        <Dialog
+          className="lg:hidden"
+          onClose={() => {
+            setIsMobileMenuOpen(false);
+          }}
+        >
+          <DialogPanel className="fixed inset-y-0 z-50 w-full overflow-y-auto bg-white px-6 py-6">
+            <DialogTitle>
+              <div className="flex flex-row items-center justify-between">
+                <Link to="/" className="-m-1.5 p-1.5">
+                  <StaticImage
+                    className=""
+                    height={40}
+                    src="../images/aycandoo-black-logo.svg"
+                    alt=""
+                  />
+                  <span className="sr-only">
+                    {data?.site?.siteMetadata?.title}
+                  </span>
+                </Link>
+                <button
+                  type="button"
+                  className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                  onClick={() => {
+                    setIsMobileMenuOpen(false);
+                  }}
+                >
+                  <span className="sr-only">Close menu</span>
+                  <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
+              </div>
+            </DialogTitle>
+            <div className="mt-6 flow-root">
+              <div className="-my-6 divide-y divide-gray-500/10">
+                <div className="space-y-2 py-6">
+                  {navigation.map(({ name, to }) => (
+                    <Link
+                      onClick={() => {
+                        setIsMobileMenuOpen(false);
+                      }}
+                      key={name}
+                      to={to}
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    >
+                      {name}
+                    </Link>
+                  ))}
+                </div>
               </div>
             </div>
-          </div>
-        </Dialog.Panel>
-      </Dialog>
+          </DialogPanel>
+        </Dialog>
+      </Transition>
     </header>
   );
 };
